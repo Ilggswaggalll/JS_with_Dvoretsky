46cdac291df41f43a25cdb83a3b110558d62eeee
Ilggswaggalll <162141227+Ilggswaggalll@users.noreply.github.com>
2025-09-18 12:01:20 +0300

    commiting the 4 and 5 task

---
+++ Task_4/index.js
@@ -0,0 +1,4 @@
+
+let func = (number) => number % 2 === 0 ? "even" : "odd";
+console.log(func(3));
+console.log(func(10));
---
+++ Task_5/index.js
@@ -0,0 +1,3 @@
+
+let theCentury = (year) => Math.ceil(year/100);
+console.log(theCentury(2025));
